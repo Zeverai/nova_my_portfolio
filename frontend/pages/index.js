885e4bcae1859e26036960ccba79e33dee42d531
Zeverai <zeveraimusic@gmail.com>
2020-06-25 13:34:36 -0400

    Arthur instructions Thursday, Jun 25.

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,37 @@
-import React from 'react'
-import Card from 'components/Card/Card'
+import React from 'react';
+import Card from 'components/Card/Card';
+import fetch from 'isomorphic-unfetch';
+
 
 export default function Index () {
     return (
         <div>
             <Card>
-                Lets get to building
+                Lets get to building.
             </Card>
         </div>
     )
 }
+
+Index.getInitialProps= async () => {
+
+    let res, data, projects;
+    try {
+        // make api request here
+        // fetch returns a promise
+        res = await fetch('http://142.93.61.248:8000/api/v1/project/');
+        data = await res.json();
+        projects = data.objects;
+
+    } catch(error) {
+
+        console.error(error)
+
+    } 
+
+    return {
+        
+        projects
+
+    }  
+};
